995a5e94132f64c3ac20cdc40484adb36e31b900
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2019-11-03 20:18:38 +0100

    render product

--- src/js/script.js
+++ src/js/script.js
@@ -94,12 +94,14 @@
     renderInMenu(){
       const thisProduct = this;
       /*generate HTML based on template*/
-
+      const generatedHTML = templates.menuProduct(thisProduct.data);
       /*create element using utils.createElementFromHTML */
-
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       /* find menu container*/
-
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      console.log('menuContainer:', menuContainer);
       /*add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
   app.init();
