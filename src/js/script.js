f39b8db8f8d22f414da8bc2dd9e0da555692c104
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2019-11-05 12:37:35 +0100

    Add getElements() in Product

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
 
       //console.log('new Product:', thisProduct);
@@ -78,14 +79,24 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+  const thisProduct = this;
+
+  thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+  thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+  thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+  thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+  thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+}
+
     initAccordion() {
       const thisProduct = this;
       //console.log('thisinitAccordion:', thisProduct);
       /* find the clickable trigger (the element that should react to clicking) */
-      const trigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      //const trigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       //console.log('clickableTrigger:', clickableTrigger);
       /* START: click event listener to trigger */
-      trigger.addEventListener('click', function () {
+      thisProduct.accordionTrigger.addEventListener('click', function () {
         //console.log('clicked', event);
         /* prevent default action for event */
         event.preventDefault();
