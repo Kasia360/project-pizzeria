2976cabf1c931507b54607cd291b7225d313ee35
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2019-11-09 16:35:11 +0100

    Add and replace files to the project

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@
   const select = {
     templateOf: {
       menuProduct: '#template-menu-product',
+      cartProduct: '#template-cart-product', // CODE ADDED
     },
     containerOf: {
       menu: '#product-list',
@@ -26,11 +27,31 @@
     },
     widgets: {
       amount: {
-        input: 'input[name="amount"]',
+        input: 'input.amount', // CODE CHANGED
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
     },
+    // CODE ADDED START
+    cart: {
+    productList: '.cart__order-summary',
+    toggleTrigger: '.cart__summary',
+    totalNumber: `.cart__total-number`,
+    totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+    form: '.cart__order',
+    formSubmit: '.cart__order [type="submit"]',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
+  },
+  cartProduct: {
+    amountWidget: '.widget-amount',
+    price: '.cart__product-price',
+    edit: '[href="#edit"]',
+    remove: '[href="#remove"]',
+  },
+  // CODE ADDED END
   };
 
   const classNames = {
@@ -38,22 +59,33 @@
       wrapperActive: 'active',
       imageVisible: 'active',
     },
+    // CODE ADDED START
+    cart: {
+    wrapperActive: 'active',
+  },
+  // CODE ADDED END
   };
 
   const settings = {
     amountWidget: {
-      defaultValue: 5,
+      defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
-  };
+    },// CODE CHANGED
+    // CODE ADDED START
+    cart: {
+    defaultDeliveryFee: 20,
+  },
+  // CODE ADDED END
+};
 
   const templates = {
     menuProduct:
     Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    // CODE ADDED START
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    // CODE ADDED END
   };
-
-
 
   class Product{
     constructor(id, data){
@@ -66,7 +98,7 @@
       thisProduct.initOrderForm();
       thisProduct.initAmountWidget();
       thisProduct.processOrder();
-      console.log('new Product:', thisProduct);
+      //console.log('new Product:', thisProduct);
     }
     renderInMenu(){
       const thisProduct = this;
@@ -83,7 +115,6 @@
 
     getElements(){
       const thisProduct = this;
-
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
@@ -101,7 +132,7 @@
       //console.log('clickableTrigger:', clickableTrigger);
       /* START: click event listener to trigger */
       thisProduct.accordionTrigger.addEventListener('click', function () {
-        console.log('clicked', event);
+        //console.log('clicked', event);
         /* prevent default action for event */
         event.preventDefault();
         //console.log(event);
@@ -112,7 +143,7 @@
         //console.log('activeProducts:', activeProducts);
         /* START LOOP: for each active product */
         for (let activeProduct of activeProducts) {
-          console.log('activeProduct:', activeProduct);
+          //console.log('activeProduct:', activeProduct);
           /* START: if the active product isn't the element of thisProduct */
           if (activeProduct !== thisProduct.element) {
             //console.log('!=activeProduct', activeProduct);
@@ -130,7 +161,7 @@
 
     initOrderForm(){
       const thisProduct = this;
-      console.log('thisProduct of initOrderFrom():', thisProduct);
+      //console.log('thisProduct of initOrderFrom():', thisProduct);
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
         thisProduct.processOrder();
@@ -150,9 +181,9 @@
 
     processOrder(){
       const thisProduct = this;
-      console.log('thisProduct of processOrder():', thisProduct);
+      //console.log('thisProduct of processOrder():', thisProduct);
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData:', formData);
+      //console.log('formData:', formData);
       /* set variable price to equal thisProduct.data.price */
       let totalPrice = thisProduct.data.price;
       /* START LOOP: for each paramId in thisProduct.data.params */
@@ -194,7 +225,7 @@
       // multiply totalPrice by amount
       totalPrice *= thisProduct.amountWidget.value;
       /* set the contents of thisProduct.priceElem to be the value of variable price */
-      console.log('totalPrice:', totalPrice);
+      //console.log('totalPrice:', totalPrice);
       thisProduct.priceElem = totalPrice;
     }
     initAmountWidget(){
@@ -213,18 +244,17 @@
       thisWidget.value = settings.amountWidget.defaultValue;
       thisWidget.setValue(thisWidget.input.value);
       thisWidget.initActions();
-      console.log('AmountWidget:', thisWidget);
-      console.log('contstructor arguments:', element);
+      //console.log('AmountWidget:', thisWidget);
+      //console.log('contstructor arguments:', element);
     }
     getElements(element){
-  const thisWidget = this;
-
-  thisWidget.element = element;
-  thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-  thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-  thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-}
-setValue(value){
+      const thisWidget = this;
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    setValue(value){
       const thisWidget = this;
       const newValue = parseInt(value);
       // TODO: Add validation
@@ -258,7 +288,7 @@ setValue(value){
   const app = {
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      //console.log('thisApp.data:', thisApp.data);
       for(let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
