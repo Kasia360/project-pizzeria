8a37b52098dd39ad887627cb09f3a8ddf50cda37
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2019-11-10 19:03:06 +0100

    add initCart

--- src/js/script.js
+++ src/js/script.js
@@ -314,16 +314,23 @@
 
       thisApp.data = dataSource;
     },
+    initCart: function(){
+          const thisApp = this;
+          const cartElem = document.querySelector(select.containerOf.cart);
+          thisApp.cart = new Cart(cartElem);
+        },
+
 
     init: function(){
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+      //console.log('*** App starting ***');
+      //console.log('thisApp:', thisApp);
+      //console.log('classNames:', classNames);
+      //console.log('settings:', settings);
+      //console.log('templates:', templates);
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
