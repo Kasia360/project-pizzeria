55f0fa0202efb9d06ad214397c76e559b35da54d
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2019-12-02 20:58:47 +0100

    add values in payload/getData

--- src/js/script.js
+++ src/js/script.js
@@ -332,6 +332,8 @@
         thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
       }
       thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+      thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
+      thisCart.dom.address = thisCart.dom.wrapper.querySelector(select.cart.address);
     }
 
     initActions(){
@@ -396,9 +398,19 @@
       const url = settings.db.url + '/' + settings.db.order;
 
       const payload = {
-        address: 'test',
+        phone: thisCart.dom.phone.value,
+        address: thisCart.dom.address.value,
+        totalNumber: thisCart.totalNumber,
+        subtotalPrice: thisCart.subtotalPrice,
+        deliveryFee: thisCart.deliveryFee,
         totalPrice: thisCart.totalPrice,
+        products: [],
       };
+
+      for (let product of thisCart.products) {
+        const productData = product.getData();
+        payload.products.push(productData);
+      }
 
       const options = {
         method: 'POST',
@@ -477,6 +489,19 @@
         thisCartProduct.remove();
       });
     }
+
+    getData() {
+      const thisCartProduct = this;
+      const productData = {
+        id: thisCartProduct.id,
+        amount: thisCartProduct.amount,
+        price: thisCartProduct.price,
+        priceSingle: thisCartProduct.priceSingle,
+        params: thisCartProduct.params,
+      };
+      return productData;
+    }
+
   }
 
   const app = {
